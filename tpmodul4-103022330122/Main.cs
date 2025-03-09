b401cfcd21822e43458bc0df5f834293072b1b58
Benaya Giuseppe Linggaputra Sinulingga <Benaya.gls@gmail.com>
2025-03-09 23:12:46 +0700

    menambah implementasi dengan table-driven

---
+++ tpmodul4-103022330122/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace tpmodul4_103022330122
+{
+    class kodepos
+    {
+        static void Main()
+        {
+            KodePos kodePos = new KodePos();
+
+            Console.WriteLine("Input Kelurahan: ");
+
+            foreach (string kelurahan in new string[]
+            {
+                "Batununggal", "Kujangsari", "Mengger", "Wates", "Cijagra", "Jatisari", "Margasari", "Sekejati",
+                "Kebonwaru", "Maleer", "Samoja"
+            })
+            {
+                Console.WriteLine($"{kelurahan}: {kodePos.GetKodePos(kelurahan)}");
+            }
+        }
+    }
+}
--- tpmodul4-103022330122/Program.cs
+++ tpmodul4-103022330122/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022330122
+{
+    class KodePos
+    {
+        private static Dictionary<string, string> TKodepos = new Dictionary<string, string>
+    {
+        {"Batununggal", "40266"},
+        {"Kujangsari", "40287"},
+        {"Mengger", "40267"},
+        {"Wates", "40256"},
+        {"Cijaura", "40287"},
+        {"Jatisari", "40286"},
+        {"Margasari", "40286"},
+        {"Sekejati", "40286"},
+        {"Kebonwaru", "40272"},
+        {"Maleer", "40274"},
+        {"Samoja", "40273"}
+    };
+        public string GetKodePos(string kelurahan)
+        {
+            if (TKodepos.ContainsKey(kelurahan))
+            {
+                return TKodepos[kelurahan];
+            }
+            return "Kodepos tidak ada";
+        }
+    }
+}
